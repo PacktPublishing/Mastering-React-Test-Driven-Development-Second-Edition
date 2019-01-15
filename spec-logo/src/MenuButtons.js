06c2add828039e9240df3cc444b56cbb571a55df
Daniel Irvine <daniel@danielirvine.com>
2019-01-15 10:23:33 +0000

    Add reset button

--- spec-logo/src/MenuButtons.js
+++ spec-logo/src/MenuButtons.js
@@ -1,5 +1,26 @@
 import React from "react";
+import {
+  useSelector,
+  useDispatch,
+} from "react-redux";
+import { initialState } from "./parser";
+
+const reset = () => ({ type: "RESET" });
 
 export const MenuButtons = () => {
-  return null;
+  const { nextInstructionId } = useSelector(
+    ({ script }) => script
+  );
+  const dispatch = useDispatch();
+
+  const canReset = nextInstructionId !== 0;
+
+  return (
+    <button
+      onClick={() => dispatch(reset())}
+      disabled={!canReset}
+    >
+      Reset
+    </button>
+  );
 };

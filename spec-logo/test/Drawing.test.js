9ce22ec0a183b259abf1c4930f71efb581479200
Daniel Irvine <daniel@danielirvine.com>
2019-02-03 01:25:59 +0000

    Add isAnimating to Display

--- spec-logo/test/Drawing.test.js
+++ spec-logo/test/Drawing.test.js
@@ -368,4 +368,35 @@ describe("Drawing", () => {
       });
     });
   });
+
+  describe("isAnimating", () => {
+    it("initially does not have the isAnimating class set", () => {
+      renderWithStore(<Drawing />, {
+        script: { drawCommands: [] },
+      });
+      expect(element("#viewport")).not.toHaveClass(
+        "isAnimating"
+      );
+    });
+
+    it("adds isAnimating class to viewport when animation begins", () => {
+      renderWithStore(<Drawing />, {
+        script: { drawCommands: [horizontalLine] },
+      });
+      triggerRequestAnimationFrame(0);
+      expect(element("#viewport")).toHaveClass(
+        "isAnimating"
+      );
+    });
+
+    it("removes isAnimating class when animation is finished", () => {
+      renderWithStore(<Drawing />, {
+        script: { drawCommands: [horizontalLine] },
+      });
+      triggerAnimationSequence([0, 500]);
+      expect(element("#viewport")).not.toHaveClass(
+        "isAnimating"
+      );
+    });
+  });
 });

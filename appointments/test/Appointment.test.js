77bf3ea7b5811820ba787d5820fc33f9bd919a71
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 09:12:04 +0000

    Render firstName of customer

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -6,12 +6,35 @@ import { Appointment } from "../src/Appointment";
 describe("Appointment", () => {
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
+    const component = (
+      <Appointment customer={customer} />
+    );
     const container = document.createElement("div");
-    document.body.appendChild(container);
+    document.body.replaceChildren(container);
 
-    act(() => ReactDOM.createRoot(container).render(component));
+    act(() =>
+      ReactDOM.createRoot(container).render(component)
+    );
 
-    expect(document.body.textContent).toContain("Ashley");
+    expect(document.body.textContent).toContain(
+      "Ashley"
+    );
+  });
+
+  it("renders another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    const component = (
+      <Appointment customer={customer} />
+    );
+    const container = document.createElement("div");
+    document.body.replaceChildren(container);
+
+    act(() =>
+      ReactDOM.createRoot(container).render(component)
+    );
+
+    expect(document.body.textContent).toContain(
+      "Jordan"
+    );
   });
 });

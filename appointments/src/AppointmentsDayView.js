0421528abde79367dad1d492487d3d4b63014fef
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 12:55:47 +0000

    Add lastName, phoneNumber, stylist, service and notes fields

--- appointments/src/AppointmentsDayView.js
+++ appointments/src/AppointmentsDayView.js
@@ -7,8 +7,45 @@ const appointmentTimeOfDay = (startsAt) => {
   return `${h}:${m}`;
 };
 
-export const Appointment = ({ customer }) => (
-  <div>{customer.firstName}</div>
+export const Appointment = ({
+  customer,
+  service,
+  stylist,
+  notes,
+  startsAt,
+}) => (
+  <div id="appointmentView">
+    <h3>
+      Today&rsquo;s appointment at{" "}
+      {appointmentTimeOfDay(startsAt)}
+    </h3>
+    <table>
+      <tbody>
+        <tr>
+          <td>Customer</td>
+          <td>
+            {customer.firstName} {customer.lastName}
+          </td>
+        </tr>
+        <tr>
+          <td>Phone number</td>
+          <td>{customer.phoneNumber}</td>
+        </tr>
+        <tr>
+          <td>Stylist</td>
+          <td>{stylist}</td>
+        </tr>
+        <tr>
+          <td>Service</td>
+          <td>{service}</td>
+        </tr>
+        <tr>
+          <td>Notes</td>
+          <td>{notes}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 );
 
 export const AppointmentsDayView = ({
@@ -25,6 +62,11 @@ export const AppointmentsDayView = ({
         {appointments.map((appointment, i) => (
           <li key={appointment.startsAt}>
             <button
+              className={
+                i === selectedAppointment
+                  ? "toggled"
+                  : ""
+              }
               type="button"
               onClick={() =>
                 setSelectedAppointment(i)

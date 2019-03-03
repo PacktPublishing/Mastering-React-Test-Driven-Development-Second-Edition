6e60d7ca62a8b5dc2fb75c49b7a1133b5ab3fff2
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 09:17:38 +0000

    Dry up tests

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -4,24 +4,26 @@ import { act } from "react-dom/test-utils";
 import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
-  it("renders the customer first name", () => {
-    const customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
+  let container;
 
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
     act(() => ReactDOM.createRoot(container).render(component));
 
+  it("renders the customer first name", () => {
+    const customer = { firstName: "Ashley" };
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Ashley");
   });
 
   it("renders another customer first name", () => {
     const customer = { firstName: "Jordan" };
-    const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
 
-    act(() => ReactDOM.createRoot(container).render(component));
+    render(<Appointment customer={customer} />);
 
     expect(document.body.textContent).toContain("Jordan");
   });

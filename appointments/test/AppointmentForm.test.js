212b8466819e5a4553d27f51b67fa55e516e730f
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 22:35:17 +0000

    Add service dropdown options and defaultProps

--- appointments/test/AppointmentForm.test.js
+++ appointments/test/AppointmentForm.test.js
@@ -12,6 +12,9 @@ describe("AppointmentForm", () => {
     initializeReactContainer();
   });
 
+  const labelsOfAllOptions = (element) =>
+    Array.from(element.childNodes, (node) => node.textContent);
+
   it("renders a form", () => {
     render(<AppointmentForm />);
     expect(form()).not.toBeNull();
@@ -22,6 +25,22 @@ describe("AppointmentForm", () => {
       render(<AppointmentForm />);
       expect(field("service")).not.toBeNull();
       expect(field("service").tagName).toEqual("SELECT");
+    });
+
+    it("has a blank value as the first value", () => {
+      render(<AppointmentForm />);
+      const firstOption = field("service").childNodes[0];
+      expect(firstOption.value).toEqual("");
+    });
+
+    it("lists all salon services", () => {
+      const services = ["Cut", "Blow-dry"];
+
+      render(<AppointmentForm selectableServices={services} />);
+
+      expect(labelsOfAllOptions(field("service"))).toEqual(
+        expect.arrayContaining(services)
+      );
     });
   });
 });

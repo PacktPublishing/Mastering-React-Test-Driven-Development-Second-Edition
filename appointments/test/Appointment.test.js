26d42721aec45e4589c524f5ee4198e1896e4329
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 09:12:04 +0000

    Render firstName of customer

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -8,10 +8,21 @@ describe("Appointment", () => {
     const customer = { firstName: "Ashley" };
     const component = <Appointment customer={customer} />;
     const container = document.createElement("div");
-    document.body.appendChild(container);
+    document.body.replaceChildren(container);
 
     act(() => ReactDOM.createRoot(container).render(component));
 
     expect(document.body.textContent).toContain("Ashley");
+  });
+
+  it("renders another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+    document.body.replaceChildren(container);
+
+    act(() => ReactDOM.createRoot(container).render(component));
+
+    expect(document.body.textContent).toContain("Jordan");
   });
 });

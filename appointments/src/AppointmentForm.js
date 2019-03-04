60f31684ddff3ba05948cdd7f5e8ddf8f61d2037
Daniel Irvine <daniel@danielirvine.com>
2019-03-04 21:17:23 +0000

    Show a table with time slots

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,20 +1,95 @@
 import React from "react";
 
+const timeIncrements = (numTimes, startTime, increment) =>
+  Array(numTimes)
+    .fill([startTime])
+    .reduce((acc, _, i) =>
+      acc.concat([startTime + i * increment])
+    );
+
+const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
+  const totalSlots = (salonClosesAt - salonOpensAt) * 2;
+  const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
+  const increment = 30 * 60 * 1000;
+  return timeIncrements(totalSlots, startTime, increment);
+};
+
+const weeklyDateValues = (startDate) => {
+  const midnight = startDate.setHours(0, 0, 0, 0);
+  const increment = 24 * 60 * 60 * 1000;
+  return timeIncrements(7, midnight, increment);
+};
+
+const toShortDate = (timestamp) => {
+  const [day, , dayOfMonth] = new Date(timestamp)
+    .toDateString()
+    .split(" ");
+  return `${day} ${dayOfMonth}`;
+};
+
+const toTimeValue = (timestamp) =>
+  new Date(timestamp).toTimeString().substring(0, 5);
+
+const TimeSlotTable = ({
+  salonOpensAt,
+  salonClosesAt,
+  today,
+}) => {
+  const dates = weeklyDateValues(today);
+  const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt);
+  return (
+    <table id="time-slots">
+      <thead>
+        <tr>
+          <th />
+          {dates.map((d) => (
+            <th key={d}>{toShortDate(d)}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {timeSlots.map((timeSlot) => (
+          <tr key={timeSlot}>
+            <th>{toTimeValue(timeSlot)}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
 export const AppointmentForm = ({
   original,
   selectableServices,
+  salonOpensAt,
+  salonClosesAt,
+  today,
 }) => (
   <form>
-    <select name="service" value={original.service} readOnly>
+    <label htmlFor="service">Salon service</label>
+    <select
+      name="service"
+      id="service"
+      value={original.service}
+      readOnly
+    >
       <option />
       {selectableServices.map((s) => (
         <option key={s}>{s}</option>
       ))}
     </select>
+    <TimeSlotTable
+      salonOpensAt={salonOpensAt}
+      salonClosesAt={salonClosesAt}
+      today={today}
+    />
   </form>
 );
 
 AppointmentForm.defaultProps = {
+  today: new Date(),
+  salonOpensAt: 9,
+  salonClosesAt: 19,
   selectableServices: [
     "Cut",
     "Blow-dry",
--- appointments/test/AppointmentForm.test.js
+++ appointments/test/AppointmentForm.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import {
   initializeReactContainer,
   render,
+  element,
+  elements,
   form,
   field,
 } from "./reactTestExtensions";
@@ -72,6 +74,48 @@ describe("AppointmentForm", () => {
       );
       const option = findOption(field("service"), "Blow-dry");
       expect(option.selected).toBe(true);
+    });
+  });
+
+  describe("time slot table", () => {
+    it("renders a table for time slots with an id", () => {
+      render(<AppointmentForm original={blankAppointment} />);
+      expect(element("table#time-slots")).not.toBeNull();
+    });
+
+    it("renders a time slot for every half an hour between open and close times", () => {
+      render(
+        <AppointmentForm
+          original={blankAppointment}
+          salonOpensAt={9}
+          salonClosesAt={11}
+        />
+      );
+      const timesOfDayHeadings = elements("tbody >* th");
+      expect(timesOfDayHeadings[0]).toContainText("09:00");
+      expect(timesOfDayHeadings[1]).toContainText("09:30");
+      expect(timesOfDayHeadings[3]).toContainText("10:30");
+    });
+
+    it("renders an empty cell at the start of the header row", () => {
+      render(<AppointmentForm original={blankAppointment} />);
+      const headerRow = element("thead > tr");
+      expect(headerRow.firstChild).toContainText("");
+    });
+
+    it("renders a week of available dates", () => {
+      const specificDate = new Date(2018, 11, 1);
+      render(
+        <AppointmentForm
+          original={blankAppointment}
+          today={specificDate}
+        />
+      );
+      const dates = elements("thead >* th:not(:first-child)");
+      expect(dates).toHaveLength(7);
+      expect(dates[0]).toContainText("Sat 01");
+      expect(dates[1]).toContainText("Sun 02");
+      expect(dates[6]).toContainText("Fri 07");
     });
   });
 });

fbbf651237d54787eecc2a85da92855369333562
Daniel Irvine <daniel@danielirvine.com>
2019-03-14 11:47:12 +0000

    Add local storage middleware

--- spec-logo/src/index.js
+++ spec-logo/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { configureStore } from "./store";
+import { configureStoreWithLocalStorage } from "./store";
 import { App } from "./App";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <Provider store={configureStore()}>
+  <Provider store={configureStoreWithLocalStorage()}>
     <App />
   </Provider>
 );

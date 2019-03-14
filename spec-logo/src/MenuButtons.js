8222790fa77736cc3f39437d6aad01d48539c6a0
Daniel Irvine <daniel@danielirvine.com>
2019-03-14 11:45:10 +0000

    Add undo and redo buttons into menu bar

--- spec-logo/src/MenuButtons.js
+++ spec-logo/src/MenuButtons.js
@@ -6,21 +6,36 @@ import {
 import { initialState } from "./parser";
 
 const reset = () => ({ type: "RESET" });
+const undo = () => ({ type: "UNDO" });
+const redo = () => ({ type: "REDO" });
 
 export const MenuButtons = () => {
-  const { nextInstructionId } = useSelector(
-    ({ script }) => script
-  );
+  const { canUndo, canRedo, nextInstructionId } =
+    useSelector(({ script }) => script);
   const dispatch = useDispatch();
 
   const canReset = nextInstructionId !== 0;
 
   return (
-    <button
-      onClick={() => dispatch(reset())}
-      disabled={!canReset}
-    >
-      Reset
-    </button>
+    <>
+      <button
+        onClick={() => dispatch(undo())}
+        disabled={!canUndo}
+      >
+        Undo
+      </button>
+      <button
+        onClick={() => dispatch(redo())}
+        disabled={!canRedo}
+      >
+        Redo
+      </button>
+      <button
+        onClick={() => dispatch(reset())}
+        disabled={!canReset}
+      >
+        Reset
+      </button>
+    </>
   );
 };

d2600d54451f17e2c1e50a1f2a2b728222c33d4a
Daniel Irvine <daniel@danielirvine.com>
2019-03-19 18:38:54 +0000

    Replace hand-rolled spy with jest.fn and jest.spyOn

--- appointments/test/domMatchers.js
+++ appointments/test/domMatchers.js
@@ -9,30 +9,3 @@ expect.extend({
   toContainText,
   toHaveClass,
 });
-
-expect.extend({
-  toBeCalledWith(received, ...expectedArguments) {
-    if (received.receivedArguments() === undefined) {
-      return {
-        pass: false,
-        message: () => "Spy was not called.",
-      };
-    }
-
-    const notMatch = !this.equals(
-      received.receivedArguments(),
-      expectedArguments
-    );
-
-    if (notMatch) {
-      return {
-        pass: false,
-        message: () =>
-          "Spy was called with the wrong arguments: " +
-          received.receivedArguments() +
-          ".",
-      };
-    }
-    return { pass: true, message: () => "Spy was called." };
-  },
-});

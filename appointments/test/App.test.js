9d2322823ae225df2b7eb5fa52837eaa399ec087
Daniel Irvine <daniel@danielirvine.com>
2019-04-06 21:17:34 +0100

    Add new button for viewing customers to search actions bar

--- appointments/test/App.test.js
+++ appointments/test/App.test.js
@@ -20,6 +20,7 @@ import { CustomerForm } from "../src/CustomerForm";
 import { blankCustomer } from "./builders/customer";
 import { blankAppointment } from "./builders/appointment";
 import { CustomerSearchRoute } from "../src/CustomerSearchRoute";
+import { CustomerHistoryRoute } from "../src/CustomerHistoryRoute";
 
 jest.mock("../src/AppointmentFormRoute", () => ({
   AppointmentFormRoute: jest.fn(() => (
@@ -37,6 +38,11 @@ jest.mock("../src/CustomerForm", () => ({
 jest.mock("../src/CustomerSearchRoute", () => ({
   CustomerSearchRoute: jest.fn(() => (
     <div id="CustomerSearchRoute" />
+  )),
+}));
+jest.mock("../src/CustomerHistoryRoute", () => ({
+  CustomerHistoryRoute: jest.fn(() => (
+    <div id="CustomerHistoryRoute" />
   )),
 }));
 
@@ -70,6 +76,13 @@ describe("App", () => {
   it("renders CustomerSearchRoute at /searchCustomers", () => {
     renderWithRouter(<App />, { location: "/searchCustomers" });
     expect(CustomerSearchRoute).toBeRendered();
+  });
+
+  it("renders CustomerHistory at /viewHistory", () => {
+    renderWithRouter(<App />, {
+      location: "/viewHistory?customer=123",
+    });
+    expect(CustomerHistoryRoute).toBeRendered();
   });
 
   const customer = { id: "123" };
@@ -126,6 +139,15 @@ describe("App", () => {
       expect(
         linkFor("/addAppointment?customer=123")
       ).toBeDefined();
+    });
+
+    it("renders a link to the /viewHistory route for each CustomerSearch row", () => {
+      renderWithRouter(<App />);
+      click(linkFor("/searchCustomers"));
+      renderWithRouter(searchFor(customer));
+      expect(
+        linkFor("/viewHistory?customer=123")
+      ).not.toBeNull();
     });
   });
 });

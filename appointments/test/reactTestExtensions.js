b872637af9754b44bc318703075432960a2e3880
Daniel Irvine <daniel@danielirvine.com>
2021-12-22 21:45:11 +0000

    Add fluent functions for accessing DOM

--- appointments/test/reactTestExtensions.js
+++ appointments/test/reactTestExtensions.js
@@ -15,3 +15,15 @@ export const render = (component) =>
 
 export const click = (element) =>
   act(() => element.click());
+
+export const element = (selector) =>
+  document.querySelector(selector);
+
+export const elements = (selector) =>
+  Array.from(document.querySelectorAll(selector));
+
+export const typesOf = (elements) =>
+  elements.map((element) => element.type);
+
+export const textOf = (elements) =>
+  elements.map((element) => element.textContent);

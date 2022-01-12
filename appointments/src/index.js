f212a38fdfb7b390c36660580e7579c6609df983
Daniel Irvine <daniel@danielirvine.com>
2022-01-12 19:36:40 +0000

    Set CustomerForm as root component

--- appointments/src/index.js
+++ appointments/src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppointmentsDayView } from "./AppointmentsDayView";
-import { sampleAppointments } from "./sampleData";
+import { CustomerForm } from "./CustomerForm";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <AppointmentsDayView
-    appointments={sampleAppointments}
-  />
+  <CustomerForm original={{}} onSubmit={() => {}} />
 );

techsachinkumar/Mastering-React-Test-Driven-Development

Add paging of search results

appointments/src/CustomerSearch.js
@@ -1,4 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+
+const SearchButtons = ({ handleNext, handlePrevious }) => (
+  <div className="button-bar">
+    <button
+      role="button"
+      id="previous-page"
+      onClick={handlePrevious}>
+      Previous
+    </button>
+    <button role="button" id="next-page" onClick={handleNext}>
+      Next
+    </button>
+  </div>
+);
 
 const CustomerRow = ({ customer }) => (
   <tr>
@@ -11,35 +25,60 @@ const CustomerRow = ({ customer }) => (
 
 export const CustomerSearch = () => {
   const [customers, setCustomers] = useState([]);
+  const [queryStrings, setQueryStrings] = useState([]);
+
+  const handleNext = useCallback(() => {
+    const after = customers[customers.length - 1].id;
+    const queryString = `?after=${after}`;
+    setQueryStrings([...queryStrings, queryString]);
+  }, [customers, queryStrings]);
+
+  const handlePrevious = useCallback(
+    () => setQueryStrings(queryStrings.slice(0, -1)),
+    [queryStrings]
+  );
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await window.fetch('/customers', {
-        method: 'GET',
-        credentials: 'same-origin',
-        headers: { 'Content-Type': 'application/json' }
-      });
+      let queryString = '';
+      if (queryStrings.length > 0)
+        queryString = queryStrings[queryStrings.length - 1];
+
+      const result = await window.fetch(
+        `/customers${queryString}`,
+        {
+          method: 'GET',
+          credentials: 'same-origin',
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
       setCustomers(await result.json());
     };
 
     fetchData();
-  }, []);
+  }, [queryStrings]);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Phone number</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {customers.map(customer => (
-          <CustomerRow customer={customer} key={customer.id} />
-        ))}
-      </tbody>
-    </table>
+    <React.Fragment>
+      <SearchButtons
+        handleNext={handleNext}
+        handlePrevious={handlePrevious}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Phone number</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map(customer => (
+            <CustomerRow customer={customer} key={customer.id} />
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 };

PacktPublishing/Mastering-React-Test-Driven-Development-Second-Edition

appointments/src/CustomerSearch.js
@@ -1,5 +1,16 @@
 import React, { useCallback, useEffect, useState } from "react";
 
+const SearchButtons = ({ handleNext, handlePrevious }) => (
+  <menu>
+    <li>
+      <button onClick={handlePrevious}>Previous</button>
+    </li>
+    <li>
+      <button onClick={handleNext}>Next</button>
+    </li>
+  </menu>
+);
+
 const CustomerRow = ({ customer }) => (
   <tr>
     <td>{customer.firstName}</td>
@@ -11,35 +22,62 @@ const CustomerRow = ({ customer }) => (
 
 export const CustomerSearch = () => {
   const [customers, setCustomers] = useState([]);
+  const [queryStrings, setQueryStrings] = useState([]);
+
+  const handleNext = useCallback(() => {
+    const after = customers[customers.length - 1].id;
+    const queryString = `?after=${after}`;
+    setQueryStrings([...queryStrings, queryString]);
+  }, [customers, queryStrings]);
+
+  const handlePrevious = useCallback(
+    () => setQueryStrings(queryStrings.slice(0, -1)),
+    [queryStrings]
+  );
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await global.fetch("/customers", {
-        method: "GET",
-        credentials: "same-origin",
-        headers: { "Content-Type": "application/json" },
-      });
+      const queryString =
+        queryStrings[queryStrings.length - 1] || "";
+
+      const result = await global.fetch(
+        `/customers${queryString}`,
+        {
+          method: "GET",
+          credentials: "same-origin",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
       setCustomers(await result.json());
     };
 
     fetchData();
-  }, []);
+  }, [queryStrings]);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Phone number</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {customers.map((customer) => (
-          <CustomerRow customer={customer} key={customer.id} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <SearchButtons
+        handleNext={handleNext}
+        handlePrevious={handlePrevious}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Phone number</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer) => (
+            <CustomerRow
+              customer={customer}
+              key={customer.id}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
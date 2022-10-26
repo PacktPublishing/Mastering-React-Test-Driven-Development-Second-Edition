03ed8bc2bd012b5b3481d0bc81dda38c5a569349
danielhawkesford <daniel.hawkesford@gmail.com>
2022-10-26 17:40:33 +0100

    Renders the customer first name

---
+++ Chapter01/Start/test/Appointment.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { act } from "react-dom/test-utils";
+import { Appointment } from "../src/Appointment";
+
+describe("Appointment", () => {
+    it("renders the customer first name", () => {
+        const customer = { firstName: "Ashley" };
+        const component = <Appointment customer={customer} />;
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+        act(() => {
+            ReactDOM.createRoot(container).render(component);
+        });
+        expect(document.body.textContent).toContain("Ashley");
+    });
+});
